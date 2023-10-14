50dd764e0bd31c54fa62efad09f88f4f03cc0823
Nike <koliaso@gmail.com>
2023-10-14 10:26:37 +0300

    relise

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { TransactionHistoryTable } from './Transactionhistory.styled';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <TransactionHistoryTable>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </TransactionHistoryTable>
+  );
+};
+
+export default TransactionHistory;
